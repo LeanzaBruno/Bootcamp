4205b8fdfa63b22ca3f50e298b2afdf9b283c0fc
BrunoLeanza <brunoadanleanza@gmail.com>
2023-05-07 12:26:44 -0300

    Modulo 4 Ejercicio 2 con PHP

---
+++ Desafios HTML y CSS/Modulo 4/Ejercicios 1 y 2/codigo.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <p>
+    Hola tu nombre es <?php echo $_POST["user"]; ?>
+    <br>
+    <br>
+    y tu contraseña es <?php echo $_POST["password"]; ?>
+    </p>
+</body>
+</html>
